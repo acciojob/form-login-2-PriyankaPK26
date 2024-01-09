8ad59fac4101021a1a1f9d8bdd9323f675d8d706
PriyankaPK26 <118288268+PriyankaPK26@users.noreply.github.com>
2024-01-09 17:36:18 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function submitForm() {
+      // Get form values
+      let firstName = document.getElementById('firstName').value;
+      let lastName = document.getElementById('lastName').value;
+      let phoneNumber = document.getElementById('phoneNumber').value;
+      let emailId = document.getElementById('emailId').value;
+
+      // Display the result using alert() with spaces
+      alert(
+       
+        "First Name: " + firstName + "\n" +
+        "Last Name: " + lastName + "\n" +
+        "Phone Number: " + phoneNumber + "\n" +
+        "Email ID: " + emailId
+      );
+    }
